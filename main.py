173e573e754e5c2789517081aea7fe19a7f51ed1
Mateo Molina Gonzalez <89753117+molxeuz@users.noreply.github.com>
2023-09-29 07:18:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import os, sys, keyboard
 import process as process
 
 # Limpiar la consola    
-
 def limpiarConsola():
-    
     os.system('cls' if os.name == 'nt' else 'clear')
     
 def mostrarMenu():
-    
     print("\n <-- Menu de opciones --> ")
     print(" 1. registro de usuario ")
     print(" 2. Rentar una bicicleta ")
@@ -17,105 +14,57 @@ def mostrarMenu():
     print(" 5. Salir \n")
     
 def finalizarPrograma():
-
     print("<-- Gracias por utilizar el software.")
-    
     print("\n" + "Presiona (Ctrl) para mostrar el menú o (Shift) para salir del programa. \n")
-
     while True:
-        
         if keyboard.is_pressed("ctrl"):
-            
             limpiarConsola()
-            
             break
-        
             mostrarMenu()
-            
         elif keyboard.is_pressed("shift"):
-            
             limpiarConsola()
-            
             print("\n" + "Saliendo del programa...\n")
-            
             sys.exit()
 
-
 opcion = "0"
-
 while opcion != "5":
-
     mostrarMenu()
-    
     opcion = input("Seleccione una opción: ")
-    
     if opcion == "1":
-    
         limpiarConsola()
-
         print("Has seleccionado la Opción 1. \n")
-        
         print("Preciona enter para ejecutar... ")
         input()
-        
         process.registrarUsuario()
-        
         finalizarPrograma()
-
     elif opcion == "2":
-        
         limpiarConsola()
-        
         print("Has seleccionado la Opción 2. \n")
-        
         print("Preciona enter para ejecutar... ")
         input()
-        
         process.registrarRenta()
-        
         finalizarPrograma()
-
     elif opcion == "3":
-        
         limpiarConsola()
-        
         print("Has seleccionado la Opción 3. \n")
-        
         print("Preciona enter para ejecutar... ")
         input()
-        
         process.consultarUsuario()
-
         finalizarPrograma()
-        
     elif opcion == "4":
-    
         limpiarConsola()
-        
         print("Has seleccionado la Opción 4. \n")
-        
         print("Preciona enter para ejecutar... ")
         input()
-        
         process.consultarRenta()
-        
         finalizarPrograma()
-        
     elif opcion == "5":
-
         limpiarConsola()
-
         print("Has seleccionado la Opción 5. \n")
-        
         print("Preciona enter para ejecutar... ")
         input()
-
         finalizarPrograma()
         sys.exit()
-
     else:
-        
         limpiarConsola()
-        
         print("\n" + "Opción no válida. Por favor, selecciona una opción del menú. \n")
-    
